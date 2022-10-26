c206dab2b2ca74ffa83c9de32a2682efe76b1658
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-10-26 20:58:26 +0200

    Add more items to item.card and to all.cards

--- src/infrastructure/components/all.cards/all.cards.tsx
+++ src/infrastructure/components/all.cards/all.cards.tsx
@@ -1,21 +1,31 @@
+import { characters } from "../../../data/characters";
 import { CardItem } from "../card.item/card.item";
 
 export function AllCards() {
     return (
         <>
-            <div className="app container">
-                <ul className="characters-list row list-unstyled"></ul>
-                <CardItem></CardItem>
-            </div>
-            <slot></slot>
-            <div className="comunications">
-                <p className="comunications__text display-1">A phrase</p>
-                <img
-                    className="comunications__picture"
-                    src="img/no-one.jpg"
-                    alt="Name and family of speaker"
-                />
-            </div>
+            {characters.map((item) => (
+                <div key={item.id} className="app container">
+                    <ul
+                        key={item.id}
+                        className="characters-list row list-unstyled"
+                    >
+                        <CardItem></CardItem>
+                    </ul>
+                    <slot></slot>
+                    <div className="comunications">
+                        <p className="comunications__text display-1">
+                            {" "}
+                            {item.message}
+                        </p>
+                        <img
+                            className="comunications__picture"
+                            src="img/no-one.jpg"
+                            alt="Name and family of speaker"
+                        />
+                    </div>
+                </div>
+            ))}
         </>
     );
 }
